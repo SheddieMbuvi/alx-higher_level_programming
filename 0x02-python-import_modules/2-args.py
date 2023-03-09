1a1123224cc2f7f923556ee87c7bb0f825a12d37
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-03-09 08:36:32 -0800

    Import argv from sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 from sys import argv
 def list_args():
-    argv_len = len(argv[1:])
+    argv_len = len(argv)
 
     if argv_len == 1:
-        print(f"{argv_len} argument:")
-        for i in range(1, argv_len + 1):
+        print("{} arguments.".format(0))
+
+    elif argv_len == 2:
+        print("{} argument:".format(argv_len - 1))
+        for i in range(1, argv_len):
             print(f"{i}: {argv[i]}")
     else:
-        print(f"{argv_len} arguments")
-        for i in range(1, argv_len + 1):
+        print("{} arguments:".format(argv_len - 1))
+        for i in range(1, argv_len):
             print(f"{i}: {argv[i]}")
 
 if __name__ == "__main__":
