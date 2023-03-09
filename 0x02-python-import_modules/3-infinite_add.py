396486e52da8fa25310a91854cb7eb0419651074
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-03-09 08:55:22 -0800

    Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    values = len(sys.argv)
+    total = 0
+    
+    for i in range(1, values):
+        total += int(sys.argv[i])
+    
+    print(total)
