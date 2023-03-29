2304f390a98a84693dd19d1ccf72c84378692b13
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-03-29 05:47:28 -0700

    Geting size as int

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,10 +8,8 @@ class Square:
     """Class that defines the size"""
     def __init__(self, size=0):
         """Method for initialization"""
-        try:
-            assert type(size) == int
-        except:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
