d532b9f7a7e73dc2456998cf726e7e78b25f3fef
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-03-29 03:05:27 -0700

    Define private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+No import modules allowed for this
+"""
+
+
+class Square:
+    """Class that defines the size"""
+    def __init__(self, size):
+        """Method for initialization"""
+        self.__size = size
