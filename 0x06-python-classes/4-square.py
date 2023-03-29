8364e19c05c6e01abe2e5ed475733b5e874e9c83
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-03-29 06:04:07 -0700

    Using setter to set a value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+No import modules allowed for this
+"""
+
+
+class Square:
+    """Class that defines the size"""
+    def __init__(self, size=0):
+        """Method for initialization"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ gets the value of __size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the value of __size """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """method that returns the current square area"""
+        return self.__size * self.__size
