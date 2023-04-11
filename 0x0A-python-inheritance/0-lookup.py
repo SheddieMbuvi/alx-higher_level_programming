4fd00deb4e5cfa5eadcbc61b8038b9f7091c2205
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-11 07:48:53 -0700

    Returns the list of available attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
-Import no modules in the function
-"""
+"""Import no modules in the function"""
 
 
 def lookup(obj):
-    """Method returning list of obj"""
+    """
+    Method returning list of obj
+    obj is mthod being lookedup
+    """
     return dir(obj)
