e11f0e1f1e92073fd40732dcd8f7a6a714ed4189
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-12 01:05:14 -0700

    Inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ check if instance is same as sub class
 
 def inherits_from(obj, a_class):
     """check if instance is same as sub class"""
-    if  issubclass(obj.__class__, a_class) is True:
+    if issubclass(obj.__class__, a_class) is True:
         if obj.__class__ is not a_class:
             return True
     return False
