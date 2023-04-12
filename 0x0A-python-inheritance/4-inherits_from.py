e32c68416a05d49812eb82de4127367def4f9493
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-12 01:03:26 -0700

    Inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Has no inport module
+check if instance is same as sub class
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if instance is same as sub class"""
+    if  issubclass(obj.__class__, a_class) is True:
+        if obj.__class__ is not a_class:
+            return True
+    return False
