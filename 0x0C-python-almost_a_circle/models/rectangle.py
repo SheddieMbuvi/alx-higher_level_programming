7b9e6af33233532c06cd498fdf1695052d21b2b9
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-17 12:35:44 -0700

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,7 +67,11 @@ class Rectangle(Base):
 
     def display(self):
         """Displays rectangle using ##"""
+        for k in range(self.__y):
+            print()
         for i in range(self.__height):
+            for li in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
