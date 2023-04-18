095bf598c2bcc3b423672ce895629e4482ad31b0
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-18 09:35:40 -0700

    Static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method that return Json string"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            json.dumps(list_dictionaries)
