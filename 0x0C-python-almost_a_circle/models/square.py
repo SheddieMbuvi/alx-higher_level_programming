fae5d520916cc1c084f54cb7ca9e8f72f249fb91
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-04-18 09:16:33 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,30 @@ class Square(Rectangle):
         """sets the size of the sqr"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes of Square"""
+        if args and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Return dictionary representation of Square"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
