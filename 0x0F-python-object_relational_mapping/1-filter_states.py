5d8de264a7c5ddc8a4fce8ddc665874014a6e1b4
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-06-16 01:01:08 -0700

    Filter by N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+prints all rows from a db
+"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    dbConnect = MySQLdb.connect(
+                                host='localhost',
+                                port=3306,
+                                user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3])
+    cur = dbConnect.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    dbConnect.close()
