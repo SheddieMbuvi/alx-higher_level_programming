5180ee1ee3b62ac43b060ef16c6b74725833ab1e
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-06-16 00:50:04 -0700

    Select all

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,15 @@
 """
 prints all rows from a db
 """
-
-
 if __name__ == '__main__':
     import sys
     import MySQLdb
-
-
-    dbConnect = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
+    dbConnect = MySQLdb.connect(
+                                host='localhost',
+                                port=3306,
+                                user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3])
     cur = dbConnect.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
